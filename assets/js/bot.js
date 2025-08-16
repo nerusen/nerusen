cda2e13342c6f165322d0eb3629323185f8dfdf1
nerusen <162538024+nerusen@users.noreply.github.com>
2025-08-16 09:00:31 +0700

    Update bot.js

--- assets/js/bot.js
+++ assets/js/bot.js
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Initialize Chat
     function initChat() {
         elements.messages.innerHTML = '';
-        addMessage("👋🏻 Welcome to <b>Nerusen AI</b>, Ini adalah bot yang dibuat oleh <b style='color:#ff0029;'>Nelsen Chandra</b>. Bot ini masih tahap pengembangan.\n\n<a href='https://github.com/nerusen' target='_blank' style='color:#00ff88; text-decoration:none;'>© Nelsen Chandra 2025</a>", 'bot');
+        addMessage("👋🏻 Welcome to <b>Nerusen AI</b>, Ini adalah bot yang dibuat oleh <b style='color:#00ff88;'>Nelsen Chandra</b>. Bot ini masih tahap pengembangan.\n\n<a href='https://github.com/nerusen' target='_blank' style='color:#ffffff; opacity:0.7; text-decoration:none; font-size:8px;'>© Nelsen Chandra 2025</a>", 'bot');
         setTimeout(() => {
             addMessage("Ketik <b style='color:#ffeb3b;'>menu</b> jika ingin melihat daftar menu", 'bot');
         }, 800);
